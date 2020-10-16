42caf365cb2671406b7c5b5ddfbc806941a3e2e5
Hugo <albrechthugo4@gmail.com>
2020-10-16 16:32:34 -0300

    novas rotas da aplicacao

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,14 +4,48 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import OrphanagesMap from '../pages/OrphanageMap';
+import OrphanageDetails from '../pages/OrphanageDetails';
+import SelectMapPosition from '../pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from '../pages/CreateOrphanage/OrphanageData';
+import Header from '../components/Header';
 
 const { Navigator, Screen } = createStackNavigator();
 
 const Routes = () => {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
-        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+      <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
+        <Screen 
+          name="OrphanagesMap" 
+          component={OrphanagesMap} 
+        />
+
+        <Screen 
+          name="OrphanageDetails" 
+          component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Detalhes" />
+          }}
+        />
+
+        <Screen 
+          name="SelectMapPosition" 
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione a posição no mapa" />
+          }}
+        />
+
+        <Screen 
+          name="OrphanageData" 
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Preencha as informações" />
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   );
